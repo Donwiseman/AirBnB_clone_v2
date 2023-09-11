1d7bd30e30f132e63c9b203ccc9bc3d2f527d68c
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-09-10 22:45:49 -0700

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 #This creates the necessary folders in webservers to serve webpages.
+apt-get update
+apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "AirBnB Webpage file format correctly Implemented" > /data/web_static/releases/test/index.html
 rm -df /data/web_static/current
 ln -s /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
+sed -i "/^\tserver_name _;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 service nginx restart
