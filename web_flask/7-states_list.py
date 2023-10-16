2c10592bfed9474c74873f966ebf34e089362ad5
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-10-16 12:43:13 -0700

    unfinished 7-states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" starts a Flask web application that displays Hello HBNB!"""
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+from models import storage, State
+
+app = Flask(__name__)
+
+# Set strict_slashes to False
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def state_list():
+    """ Displays a list of all available states """
+    states = storage.all(State)
+    return render_template('7-states_list.html', n=states)
+
+
+@app.teardown_appcontext
+def app_teardown():
+    """ Help in closing each request session. """
+    storage.close()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
