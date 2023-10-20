85232bda9f33079d4f0e027fd0bf8089d0ef43e6
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-10-19 18:13:50 -0700

    web_flask/8-cities_by_states.py

--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base, Column, Integer, String,\
         relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -9,23 +10,20 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates="state",
+    cities = relationship('City', backref="state",
                           cascade="all, delete")
 
-    @property
-    def cities(self):
-        """propery decorator for cities attribute"""
-        from models import getenv
-
-        if getenv("HBNB_TYPE_STORAGE") == 'db':
-            return self.cities
-
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         from models import storage
         from models.city import City
 
-        city_inst = storage.all(City)
-        match_cities = []
-        for key, obj in city_inst.items():
-            if self.id == obj.state_id:
-                match_cities.append(obj)
-        return match_cities
+        @property
+        def cities(self):
+            """propery decorator for cities attribute"""
+
+            city_inst = storage.all(City)
+            match_cities = []
+            for key, obj in city_inst.items():
+                if self.id == obj.state_id:
+                    match_cities.append(obj)
+            return match_cities
