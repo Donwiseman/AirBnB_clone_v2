08c1fcd84122a6c9f0ebb36f8ac7bc7e39a3f035
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-10-21 03:34:43 +0000

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" starts a Flask web application that displays Hello HBNB!"""
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+from models import storage, State
+
+app = Flask(__name__)
+
+# Set strict_slashes to False
+app.url_map.strict_slashes = False
+
+
+@app.route('/states', defaults={'id': ''})
+@app.route('/states/<id>')
+def states(id=''):
+    """Displays a list of all available states and their cities if specified"""
+    states = storage.all(State)
+    return render_template('9-states.html', n=states, id=id)
+
+
+@app.teardown_appcontext
+def app_teardown(exception=None):
+    """ Help in closing each request session. """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
